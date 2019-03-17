9dc89e80b9388c7a020333213d173f666718cc30
Denis <igehuc.dev@gmail.com>
2019-03-18 00:18:51 +0300

    some comments for the future

--- publishstatus-bitbucket.js
+++ publishstatus-bitbucket.js
@@ -1,6 +1,27 @@
 const https = require('https');
 
-https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+const args = process.argv.slice(2);
+
+// https://<bitbucket-base-url>/rest/build-status/1.0/commits/<commit-hash>
+// {
+//     "state": "<INPROGRESS|SUCCESSFUL|FAILED>",
+//     "key": "<build-key>",
+//     "name": "<build-name>",
+//     "url": "<build-url>",
+//     "description": "<build-description>"
+// }
+
+// --bitbucket-base-url    -b
+// --commit-hash           -c
+// --status                -s
+// Check ENV variables like PIPELINE_NAME, configurationName and JenkinsStuff
+// --configuration-id      -k
+// --build-url             -u
+// --build-name            -n
+// --description           -d
+
+
+https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', resp => {
     let data = '';
 
     // A chunk of data has been recieved.
